8469bf68692d1a68b7c3f7107ccf7683e9353615
Sára Kraibich Tesařová <sara.k.tesarova@gmail.com>
2024-04-11 20:17:26 +0200

    náhodná čísla

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const start = 15
 const delka = 10
 const konec = (start + delka) % 24
 document.body.innerHTML = 'konec závodu v: ' + konec
+
+
+// náhodná čísla
+
+document.body.innerHTML += '<p>nahodne cislo: ' + Math.random() + '</p>'
