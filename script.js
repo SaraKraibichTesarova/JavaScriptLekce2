96bea3684e3f5b8d934c936788b0a0224efa8e60
Sára Kraibich Tesařová <sara.k.tesarova@gmail.com>
2024-04-11 20:46:46 +0200

    příjem divadla

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ const wageInEur = 20
 const exchangeRate = 24.55
 const wageInCzk = Math.round(wageInEur * exchangeRate)
 document.body.innerHTML += '<h1> Mzda v korunách: ' + wageInCzk + '</h1>'
+
+
+// házení kostkou
+
+let kostka = Math.round(Math.random() * 6) + 1
+document.body.innerHTML += '<p>hod kostkou: ' + kostka + '</p>'
+
+
+// příjem divadla
+
+const listek = 12
+const pocetNavstevniku = 174
+const pocetPredstaveni = 15
+const prijem = listek * pocetNavstevniku * pocetPredstaveni
+document.body.innerHTML += '<p> Prijem divadla: ' + prijem + '</p>'
+const listekStudent = 0.65 * listek
+const pocetStudentu = 0.4 * pocetNavstevniku
+const pocetDospelych = pocetNavstevniku - pocetStudentu
+const prijemSeStudenty = (listek * pocetDospelych * pocetPredstaveni) + (listekStudent * pocetStudentu * pocetPredstaveni)
+document.body.innerHTML += '<p> Prijem divadla se studenty: ' +  prijemSeStudenty + '</p>'
